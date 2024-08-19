0bbaf24869b19d8c7833118c92acd61b3b596dda
parv nema <115030244+PARVNEMA@users.noreply.github.com>
2024-08-19 08:00:22 +0530

    new changes

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
+import fs from "fs";
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary=async(localfilepath){
+  try {
+    if(!localfilepath)return null;
+
+    const res=await cloudinary.uploader.upload(localfilepath,{resource_type:"auto"})
+    console.log(res.url);
+    return res;
+  } catch (error) {
+    fs.unlinkSync(localfilepath);
+    return null;
+  }
+}
+
+
+export { uploadOnCloudinary}
